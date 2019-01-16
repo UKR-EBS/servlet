999cc7b39d245195b5caffafc0ed9f508103de15
UKR-EBS <egorovtrans@gmail.com>
2019-01-16 21:25:57 +0200

    create new project 'servlet'

---
+++ src/main/java/ServletSimple.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/temp")
+public class ServletSimple extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("<h1>Hello Servlet in Maven project</h1>");
+    }
+}
